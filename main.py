c312f6849acaad759cb894b924e179f537d46e02
AbdulazizAlkhars <alkharswork@gmail.com>
2022-07-05 19:08:56 +0300

    Task Done

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+
+listOfTemp = [100, 80, 90, 70]
+thresHold = 20
+
 # printer(elements)
 # - Accepts a list
 # - Prints every element of the list
+
+
 def printer(elements):
     # Your code here
-    ...
+    for element in elements:
+        print(element)
 
 
 # to_celsius(temperatures)
@@ -15,7 +22,13 @@ def printer(elements):
 #   C = (F - 32) * (5/9)
 def to_celsius(temperatures):
     # Your code here
-    ...
+
+    listInC = []
+    for tempreture in temperatures:
+        tempretureInCelsius = (tempreture-32)*(5/9)
+        listInC.append(tempretureInCelsius)
+
+    return listInC
 
 
 # hottest_days(temperatures, threshold)
@@ -25,7 +38,13 @@ def to_celsius(temperatures):
 #   that exceed the threshold
 def hottest_days(temperatures, threshold):
     # Your code here
-    ...
+    listOfHot = []
+    for hotTempreture in temperatures:
+        if hotTempreture > threshold:
+            listOfHot.append(hotTempreture)
+        else:
+            None
+    return listOfHot
 
 
 # log_hottest_days(temperatures, threshhold)
@@ -39,4 +58,10 @@ def hottest_days(temperatures, threshold):
 #       all previous functions
 def print_hottest_days(temperatures, threshhold):
     # Your code here
-    ...
+    listOfCel = to_celsius(temperatures=listOfTemp)
+    hottestList = hottest_days(temperatures=listOfCel, threshold=thresHold)
+    print(hottestList)
+
+
+printer(listOfTemp)
+print_hottest_days(temperatures=listOfTemp, threshhold=thresHold)
